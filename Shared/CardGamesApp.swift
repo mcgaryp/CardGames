90405d9b8b8939456be4b72d800e8fdda9eb9c45
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2022-07-09 13:23:48 -0700

    Initial Commit

---
+++ Shared/CardGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGamesApp.swift
+//  Shared
+//
+//  Created by Porter McGary on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CardGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
